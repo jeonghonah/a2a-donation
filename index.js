7dc7a46dac1c2d42e0b23d6fc138ba5edb5b1f28
Albert Nah <albert.nah@groundx.xyz>
2021-01-04 22:38:43 +0900

    add qr code

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const { prepare, request } = klipSDK
-
 (async () => {
+    const { prepare, request } = klipSDK
+
     const urlParams = new URLSearchParams(window.location.search);
     const bappName = urlParams.get('name');
     const to = urlParams.get('to');
     const amount = urlParams.get('amount');
 
+    let request_key
+
     try {
-        const { request_key } = await prepare.sendKLAY({ bappName, to, amount })
-        request(request_key)
+        ({ request_key } = await prepare.sendKLAY({ bappName, to, amount }))
     } catch (err) {
         console.error(err)
+        return
     }
+
+    const showQRCode = () => {
+        document.getElementById("container").style.visibility = "visible";
+        var qrcode = new QRCode("qrcode");
+        qrcode.makeCode("https://klipwallet.com/?target=/a2a?request_key="+request_key)
+    }
+
+    request(request_key, showQRCode)
 })()
